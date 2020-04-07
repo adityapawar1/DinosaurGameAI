1e4e8575e4e53d7e674d89d340b94b4ccb6e3a5a
Aditya Pawar <adityapawar@Adityas-MacBook-Pro.local>
2020-04-06 20:01:06 -0700

    added all number templates

---
+++ ManualOCR/template_matching.py
@@ -0,0 +1,14 @@
+import cv2 as cv
+import numpy as np
+from matplotlib import pyplot as plt
+
+img_rgb = cv.imread('scores/score0.png')
+img_gray = cv.cvtColor(img_rgb, cv.COLOR_BGR2GRAY)
+template = cv.imread('scores/score1.png',0)
+w, h = template.shape[::-1]
+res = cv.matchTemplate(img_gray,template,cv.TM_CCOEFF_NORMED)
+threshold = 0.95
+loc = np.where( res >= threshold)
+for pt in zip(*loc[::-1]):
+    cv.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+cv.imwrite('res.png',img_rgb)
